aa7d7e8ed7245159e8ce65c5ef3bc5b7cbaaecdb
masaaki.sawa <gomadesk@gmail.com>
2020-12-30 06:55:29 +0000

    user signup

---
+++ app/controllers/index_controller.rb
@@ -0,0 +1,10 @@
+class IndexController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_30_032302) do
+ActiveRecord::Schema.define(version: 2020_12_30_040547) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
     t.string "content"
     t.string "img"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
